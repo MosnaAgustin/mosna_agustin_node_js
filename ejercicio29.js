cf82d01f4ffbdf617cabfcab5630b004c9a6a752
MosnaAgustin <135614721+MosnaAgustin@users.noreply.github.com>
2023-06-07 08:44:13 -0300

    Add files via upload
    ejercicio-29-node-js

---
+++ ejercicio29.js
@@ -0,0 +1,13 @@
+const prompt= require('prompt-sync')();
+var a=parseInt(prompt("Introduzca un numero: "));
+Array1=[]
+function contarHasta(a){
+    i=0
+    while (i<a){
+        for (let i=1;i<=a;i++){
+        console.log(i)
+    }
+    break
+}
+}
+console.log(contarHasta(a))
