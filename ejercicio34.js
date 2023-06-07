047ce5594db80a069122ca16ccd3c9ffa263c14e
MosnaAgustin <135614721+MosnaAgustin@users.noreply.github.com>
2023-06-07 08:47:24 -0300

    Add files via upload
    ejercicio-34-node-js

---
+++ ejercicio34.js
@@ -0,0 +1,27 @@
+const prompt= require('prompt-sync')();
+var paises = [
+  {nombre: "argentina", continente: "sudamerica",  poblacion: 40000000},
+  {nombre: "brasil", continente: "sudamerica", poblacion: 300000000},
+  {nombre: "venezuela", continente: "sudamerica", poblacion: 25000000},
+  {nombre: "chile", continente: "sudamerica", poblacion: 10000000},
+  {nombre: "australia", continente: "oceania", poblacion: 18000000},
+  {nombre: "nueva zelanda", continente: "oceania", poblacion: 8000000},
+  {nombre: "china", continente: "asia", poblacion: 1000000000},
+  {nombre: "tailandia", continente: "asia", poblacion: 32000000},
+  {nombre: "vietnam", continente: "asia", poblacion: 23000000},
+  {nombre: "españa", continente: "europa", poblacion: 29000000},
+  {nombre: "alemania", continente: "europa", poblacion: 33000000},
+  {nombre: "francia", continente: "europa", poblacion: 65000000},
+  {nombre: "portugal", continente: "europa", poblacion: 4000000},
+  {nombre: "grecia", continente: "europa", poblacion: 12000000},
+];
+function doubleFilter(nom,cont,pob){
+array1=[];
+for(const i in nom){
+    if (nom[i].continente == cont && nom[i].poblacion >= pob) {
+        array1.push(nom[i]);
+        }
+    }
+    return array1 
+}
+console.log(doubleFilter(paises, "asia", 10000000));
