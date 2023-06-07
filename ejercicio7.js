b0c66e3c5e991d12734344281d85cbb7ad152964
MosnaAgustin <135614721+MosnaAgustin@users.noreply.github.com>
2023-06-07 08:23:09 -0300

    Add files via upload
    ejercicio-7-node-js

---
+++ ejercicio7.js
@@ -0,0 +1,7 @@
+const prompt= require('prompt-sync')();
+var a=parseInt(prompt("Introduzca un numero: "));
+var b=parseInt(prompt("Introduzca otro numero: "));
+if(a>b) console.log("el numero mas grande es: ",a);
+else{console.log("el numero mas grande es: ",b)}
+
+
