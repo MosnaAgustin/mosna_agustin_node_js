2ad837ed25f58bfbd4202b61f1137deab306136b
MosnaAgustin <135614721+MosnaAgustin@users.noreply.github.com>
2023-06-07 08:20:45 -0300

    Add files via upload
    ejercicio-3-node-js

---
+++ ejercicio3.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+var var1 = String(prompt("ingrese una palabra: ")); String;
+var var2 = String(prompt("ingrese una palabra: ")); String;
+if (var1 == var2) {console.log(" true");} else {
+    console.log(" false");} 
+if (var1.length == var2.length) {console.log(" true");} else {
+    console.log(" false");} 
